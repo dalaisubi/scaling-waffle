472bed05292282cd9ff7a59cd43b399382d60155
dalaisubi <subhendu.dalai@ceai.io>
2019-03-23 04:08:04 +0530

    cards and fetch api added

---
+++ waffle/src/card.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import './card.scss';
+
+class Card extends Component {
+    
+	render(){
+		console.log(this.props.post)
+		return (
+			<div className="card__container">
+				<div className="card__title">{this.props.post.title}</div>
+				<div className="card__body">{this.props.post.body}</div>
+			</div>
+		)
+	}
+}
+
+
+export default Card
---
+++ waffle/src/post_card.js
@@ -0,0 +1,54 @@
+import React, {Component} from 'react';
+import Card from './card';
+import "./post_card.scss"
+
+class PostCard extends Component {
+
+	constructor(props) {
+	    super(props);
+	    this.state = {
+	      error: null,
+	      isLoaded: false,
+	      items: []
+	    };
+  	}
+
+  	componentDidMount() {
+	    fetch("http://jsonplaceholder.typicode.com/posts")
+	      .then(res => res.json())
+	      .then(
+	        (result) => {
+	          this.setState({
+	            isLoaded: true,
+	            items: result
+	          });
+	        },
+	        (error) => {
+	          this.setState({
+	            isLoaded: true,
+	            error
+	          });
+	        }
+	      )
+  	}
+
+	render(){
+		console.log(this.state.items.length)
+		return (
+			<div className="postcard__wrapper">
+			    {this.state.items.length > 0 ?
+			    	this.state.items.map((data, i) => 
+				    	<div className="postcard__container" key={i}>
+							<Card post={data} />
+						</div>
+					)
+					: ""
+			    }
+				
+			</div>
+			)
+	}
+
+}
+
+export default PostCard
